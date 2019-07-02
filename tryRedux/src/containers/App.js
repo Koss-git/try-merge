6e5096d762d61af08e0b0c67fafc151e2e309333
Konstantin <savchinkonstantin@gmail.com>
2019-07-02 17:24:16 +0300

    add two components

--- tryRedux/src/containers/App.js
+++ tryRedux/src/containers/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import './App.css'
+import { User } from '../components/User'
+import { Page } from '../components/Page'
 
+import './App.css'
 
 class App extends Component {
   render() {
@@ -11,21 +13,17 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p>Привет, {user.name}!</p>
-        <p>
-          У тебя {page.photos.length} фото за {page.year} год
-        </p>
+        <User name={user.name} />
+        <Page photos={page.photos} year={page.year} />
       </div>
     )
   }
 }
 
-// приклеиваем данные из store
 const mapStateToProps = store => {
-  console.log(store) // посмотрим, что же у нас в store?
   return {
     user: store.user,
-    page : store.page
+    page: store.page,
   }
 }
 
